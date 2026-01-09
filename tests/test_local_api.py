6b9b323ee0ae072455029b54badb343b619d164c
DevYeop <lee.sangyeop0715@gmail.com>
2026-01-09 13:50:54 +0900

    뭐 이상없음~

--- tests/test_local_api.py
+++ tests/test_local_api.py
@@ -1,5 +1,8 @@
 import requests
 
+# 선택할 곳에 커서두기
+# 쉬프트
+# 페이지
 def test_local_get_hello(base_url_local):
     """
     로컬 API(/hello) GET 테스트
@@ -9,4 +12,3 @@ def test_local_get_hello(base_url_local):
     response = requests.get(url)
 
     assert response.status_code == 200
-    
